91693a28e675419e17476630909762a56ac0b8dd
swknag <jaguar11789@naver.com>
2026-07-23 16:42:57 +0900

    login_commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout')
+]
